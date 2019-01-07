ce48c1f160a57bc2891c6397a01bdba524bf109c
Bart Boerman <b.boerman@centennium.nl>
2019-01-07 16:11:04 +0100

    Work in progress.

--- h2o.R
+++ h2o.R
@@ -41,7 +41,6 @@ movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(levels(movieId))
                                            title = as.character(title),
                                            genres = as.character(genres))
 movielens <- left_join(ratings, movies, by = "movieId")
-
 # Validation set will be 10% of MovieLens data
 set.seed(1)
 test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
@@ -66,15 +65,15 @@ validation <- validation %>% select(-rating)
 
 write.csv(validation %>% select(userId, movieId) %>% mutate(rating = NA),
           "submission.csv", na = "", row.names=FALSE)
-rm(dl, ratings, movies, test_index, temp, movielens, removed)
+rm(dl, ratings, movies, test_index, temp, movielens, removed, test)
 
 # Added by Bart Boerman
 # Save object to R data format so it can be restored into a rmd file or
 # in a new R session. This will take some seconds for large files but
 # is fasther then rebuilding large objects from scratch.
 # Refer to: http://www.sthda.com/english/wiki/saving-data-into-r-data-format-rds-and-rdata
-saveRDS(validation, "validation.rds")
 saveRDS(test, "test.rds")
+saveRDS(validation, "validation.rds")
 ####################################################################################
 # Data wrangling                                                                   #    
 ####################################################################################
@@ -87,9 +86,13 @@ f_mode <- function(x, na.rm = FALSE) {
               ux <- unique(x)
               return(ux[which.max(tabulate(match(x, ux)))]) # Return mode 
 }
-# Impute missing values
-edx <- edx %>% mutate(title = if_else(is.na(title),"title unknown (1994)", title) # impute with median release year which is a arbitery chois.
-                              ,genres = if_else(is.na(genres),"genre unknown", genres))
+# Remove whitespace from start and end of string
+# Impute missing values 
+edx <- edx %>% mutate(title =  str_trim(title)
+                      ,genres = str_trim(genres)
+                      ,title = if_else(is.na(title),"title unknown (1994)", title) # impute with median release year which is a arbitery choice.
+                      ,genres = if_else(is.na(genres),"Genre.unknown", genres)
+                      ,genres = if_else(genres == "(no genres listed)","Genre.unknown", genres))
 # Add year and month based on timestamp rating
 # Removed wday and hour because of limited importance
 edx <- edx %>% mutate(datetime = lubridate::as_datetime(timestamp, origin = "1970-01-01")
@@ -102,7 +105,7 @@ edx <- edx %>% mutate(movie_releaseyear = as.numeric(str_extract(str_extract(tit
 # Count number of genres per movie
 edx <- edx %>% mutate(movie_n_genres = str_count(genres,"[|]") + 1)
 # One-hot encode genre
-genres <- c("Action","Adventure","Animation","Children","Comedy","Crime","Documentary","Drama","Fantasy","Film-Noir","Horror","Musical","Mystery","Romance","Sci-Fi","Thriller","War","Western")
+genres <- c("Action","Adventure","Animation","Children","Comedy","Crime","Documentary","Drama","Fantasy","Film-Noir","Horror","Musical","Mystery","Romance","Sci-Fi","Thriller","War","Western","IMAX","Genre.unknown")
 for (g in genres ){
       varNameDummy <- paste("movie_dummy",g, sep = "_")
       edx[[varNameDummy]] <- str_count(edx$genres,g) 
@@ -115,15 +118,11 @@ for (g in genres ){
                       select(userId, !! varNameMode) %>%
                       distinct
       edx <- left_join(x = edx, y = tmp, by = "userId")
-      edx <- edx %>% mutate(!! as.name(varNameDummy) := if_else(!! as.name(varNameDummy) == 1,!! as.name(varNameMode),0))
       rm(tmp)
 }
+edx$genres <- str_replace(edx$genres, "|IMAX", "") # We have an imax indicator column movie_dummy_IMAX
 edx <- edx %>% replace(., is.na(.), 0) # impute NA caused by left joins (users who dit not rate specific genres)
 # Add mode rating per genre, movie and user
-edx <- edx %>%
-                      group_by(genres) %>% 
-                      mutate(movie_mode_genre = f_mode(rating)) %>%
-                      ungroup
 edx <- edx %>%
                       group_by(movieId) %>% 
                       mutate(movie_mode_movie = f_mode(rating)) %>%
@@ -148,7 +147,7 @@ edx <- edx %>%
 ####################################################################################
 meta_movie <- edx %>% 
                       select(-userId, -rating, -timestamp, -datetime, -year, -month) %>% 
-                      select(movieId, title, genres, movie_releaseyear, movie_n_genres, movie_mode_movie, movie_mode_genre, movie_n_users, matches("movie_dummy")) %>%
+                      select(movieId, title, genres, movie_releaseyear, movie_n_genres, movie_mode_movie, movie_n_users, matches("movie_dummy")) %>%
                       distinct
 x <- names(meta_movie)
 meta_user <- edx %>% 
@@ -156,12 +155,17 @@ meta_user <- edx %>%
                       select(-x, userId) %>% 
                       distinct
 ####################################################################################
-# Start script from this point if you already completed data wrangling             #    
+# Save data so you can restart after data wrangling                                #    
 ####################################################################################
 saveRDS(edx, "edx.rds") # save object to R data format so it can be restored
 saveRDS(meta_movie, "meta_movie.rds") # save object to R data format so it can be restored
 saveRDS(meta_user, "meta_user.rds") # save object to R data format so it can be restored
-edx <- readRDS("edx.rds") # reload data, you can re-run script from this point forward.
+rm(edx)
+rm(meta_movie)
+rm(meta_user)
+####################################################################################
+# Start script from this point if you already completed data wrangling             #    
+####################################################################################
 ####################################################################################
 # Start h2o cloud                                                                  #    
 ####################################################################################
@@ -173,37 +177,33 @@ h2o.init(max_mem_size = "8G", #
 # Get data into cloud                                                              #    
 ####################################################################################
 # Note: this can take some minutes, time for coffee
-drop <- c("timestamp","datetime","title","month")
-
-train_h2o <- as.h2o(edx %>% select(-drop, -matches("user_mode")), 
+edx <- readRDS("edx.rds") # reload data, you can re-run script from this point forward.
+edx$rating <- as.factor(edx$rating) # for classification, response should be a factor
+edx$movieId <- as.factor(edx$movieId) # also, convert categorical (nominal) data to factor
+edx$userId <- as.factor(edx$userId)
+drop <- c("timestamp","datetime","title","genres")
+train_h2o <- as.h2o(edx %>% select(-drop), # -matches("user_mode") 
                             destination_frame = "train") # name in h2o cloud
-
-                    
-# Identify predictors (x) and response (y)
-y <- "rating"
-x <- setdiff(names(train_h2o), y) 
-
-# For classification, response should be a factor
-# Also, convert nominal data to factor
-factor <- c(y)
-train_h2o[,factor] <- as.factor(train_h2o[,factor])
+rm(edx)                    
 ####################################################################################
 # Train XGBoost                                                                    #    
 ####################################################################################
+y = "rating"
+x = setdiff(names(train_h2o),y)
 Sys.time()
 # Note: this can take some hours depening on your system. Watch a movie?
 xgb_h2o <- h2o.xgboost(x = x,
                        y = y,
                        training_frame = train_h2o,
                        distribution = "AUTO",
-                       ntrees = 50, # nround
+                       ntrees = 100, # nround
                        max_depth = 4,
-                       learn_rate = 0.3, # eta
+                       learn_rate = 0.2, # eta
                        gamma = 0,
-                       min_rows = 4,
-                       stopping_rounds = 2,
-                       stopping_metric = "misclassification", 
-                       stopping_tolerance = 0.001,
+                       min_rows = 100,
+                       stopping_rounds = 3,
+                       stopping_metric = "misclassification", # 0.42675743 max_depth = 4 + userId + MovieId
+                       stopping_tolerance = 0.01,
                        nfolds = 2,
                        fold_assignment = "Stratified",
                        tree_method = "auto",
@@ -211,54 +211,53 @@ xgb_h2o <- h2o.xgboost(x = x,
                        model_id	= "xgb_h2o",
                        seed = 1413)
 Sys.time()
-# http://docs.h2o.ai/h2o/latest-stable/h2o-docs/save-and-load-model.html
+model_path <- h2o.saveModel(xgb_h2o, path = getwd(), force=TRUE)
+saveRDS(model_path, "xgb_h2o.rds")
 varimp_xgb_h2o <- h2o.varimp(xgb_h2o)
-
-tree <- h2o.getModelTree(model = xgb_h2o, tree_number = 1, tree_class = "NO")
-
-# https://htmlpreview.github.io/?https://github.com/ledell/sldm4-h2o/blob/master/sldm4-deeplearning-h2o.html
-Sys.time()
-dnn_h2o <- h2o.deeplearning(x = x, #  0.38528666
-                            y = y,
-                            training_frame = train_h2o,
-                            epochs = 50,
-                            hidden = c(50,50,50),
-                            input_dropout_ratio = 0.1,
-                            distribution = "AUTO",
-                            nfolds = 3,
-                            score_interval = 1,
-                            stopping_rounds = 3,                   #used for early stopping
-                            stopping_metric = "misclassification", #used for early stopping
-                            stopping_tolerance = 1e-3,             #1e-3  #used for early stopping
-                            standardize = TRUE,
-                            keep_cross_validation_predictions = FALSE,
-                            model_id = "dnn_h2o",
-                            seed = 1413)
-# h2o.saveModel(dnn_h2o)
-# Sys.time()
-#h2o.shutdown()
-
-# https://www.datacamp.com/community/tutorials/keras-r-deep-learning
+# http://docs.h2o.ai/h2o/latest-stable/h2o-docs/save-and-load-model.html
+rm(train_h2o)
+rm(xgb_h2o)
+#tree <- h2o.getModelTree(model = xgb_h2o, tree_number = 1, tree_class = "NO")
 ####################################################################################
 # Test on unseen data                                                              #    
 ####################################################################################
+model_path <- readRDS("xgb_h2o.rds")
+xgb_h2o <- h2o.loadModel(model_path)
 # Data wrangling
 test <- readRDS("test.rds") %>% mutate(datetime = lubridate::as_datetime(timestamp, origin = "1970-01-01")
                                         ,year = lubridate::year(datetime)
-                                        ,month = lubridate::month(datetime)) %>%
+                                        ,month = lubridate::month(datetime)
+                                       ) %>%
                                 select(userId, movieId, rating, year, month)
 meta_movie <- readRDS("meta_movie.rds") 
 meta_user <- readRDS("meta_user.rds") 
+meta_user_genre <- readRDS("meta_user_genre.rds") 
 # Add meta data about movies and users
-test <- left_join(x = test, y = meta_user, by = "userId")
 test <- left_join(x = test, y = meta_movie, by = "movieId")
+test <- left_join(x = test, y = meta_user, by = "userId")
+test <- left_join(x = test, y = meta_user_genre, by = c("userId","genres"))
+test$rating <- as.factor(test$rating)
+test$genres <- as.factor(test$genres)
+test_set_genres <- test %>% filter(!is.na(usr_mode_genre)) # no previous data available when it is the first occurance of genre per user
+test_set_others <- test %>% filter(is.na(usr_mode_genre)) 
 # Load data into cloud
-test_h2o <- as.h2o(test, key = "test")  
-factor <- c("genres", "userId", "movieId")
-test_h2o[,factor] <- as.factor(test_h2o[,factor])
-test_h2o$prediction <- h2o.predict(xgb_h2o, newdata = test_h2o)["predict"]
+test_set_genres_h2o <- as.h2o(test_set_genres, key = "test_set_genres")  
 # Predict ratings on validation set
-test_h2o.df <- as.data.frame(test_h2o)
-confusionMatrix(table(test$rating, test$prediction))
+prediction <- h2o.predict(xgb_h2o, newdata = test_set_genres_h2o)["predict"]
+prediction <- as.data.frame(prediction)
+h2o.shutdown()
+rm(test)
+levels(prediction$predict)[levels(prediction$predict)=="1.0"] <- "1"
+levels(prediction$predict)[levels(prediction$predict)=="2.0"] <- "2"
+levels(prediction$predict)[levels(prediction$predict)=="3.0"] <- "3"
+levels(prediction$predict)[levels(prediction$predict)=="4.0"] <- "4"
+levels(prediction$predict)[levels(prediction$predict)=="5.0"] <- "5"
+
+genres_df <- edx %>%
+  separate_rows(genres, sep = "\\|") %>%
+  group_by(genres) %>%
+  summarise(number = n()) %>%
+  arrange(desc(number))
+
 
 
